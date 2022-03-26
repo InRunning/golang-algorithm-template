4bc5c4148b68ffa9609e728bee5117b399e1db29
davidlin <davidlin@futunn.com>
2022-03-26 16:10:42 +0800

    建立测试分支

--- binary_search/searchTargetIndex.go
+++ binary_search/searchTargetIndex.go
@@ -1,25 +1,5 @@
 package binary_search
 
 func search(nums []int, target int) int {
-	// 1. 初始化 start 和 end
-	start := 0
-	end := len(nums) - 1
-	// 2. 循环退出条件
-	for start+1 < end {
-		middle := (start + end) / 2
-		if nums[middle] == target {
-			return middle
-		} else if nums[middle] < target {
-			start = middle
-		} else {
-			end = middle
-		}
-	}
-	if nums[start] == target {
-		return start
-	}
-	if nums[end] == target {
-		return end
-	}
 	return -1
 }
