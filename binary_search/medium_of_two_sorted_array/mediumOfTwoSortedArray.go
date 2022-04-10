87f6ee4a05b1e7b2aa69a6d2eee1cc8f90222fd3
davidlin <davidlin@futunn.com>
2022-04-10 21:36:15 +0800

    成功解决两个有序数组找中数问题

--- binary_search/medium_of_two_sorted_array/mediumOfTwoSortedArray.go
+++ binary_search/medium_of_two_sorted_array/mediumOfTwoSortedArray.go
@@ -2,41 +2,44 @@ package medium_of_two_sorted_array
 
 func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	totalLength := len(nums1) + len(nums2)
-	// 判断奇偶
+	// 奇数时只需取中间的数
 	if totalLength%2 == 1 {
 		midIndex := totalLength / 2
-		return float64(getKthElement(nums1, nums2, midIndex+1))
-	} else {
-		midIndex1, midIndex2 := totalLength/2-1, totalLength/2
-		return float64(getKthElement(nums1, nums2, midIndex1+1)+getKthElement(nums1, nums2, midIndex2+1)) / 2.0
+		return float64(findKthElement(nums1, nums2, midIndex+1))
+	} else { // 偶数时取两个数
+		midIndex1 := totalLength/2 - 1
+		midIndex2 := totalLength / 2
+		mediumNumSum := findKthElement(nums1, nums2, midIndex1+1) + findKthElement(nums1, nums2, midIndex2+1)
+		return float64(mediumNumSum) / 2.0
 	}
 }
 
-func getKthElement(nums1, nums2 []int, k int) int {
-	index1, index2 := 0, 0
+func findKthElement(nums1 []int, nums2 []int, k int) int {
+	startIndex1, startIndex2 := 0, 0
 	for {
 		// 1. 处理特殊情况
 		// 1.1 当数组长度为0时
-		if index1 == len(nums1) {
-			return nums2[index2+k-1]
+		if startIndex1 == len(nums1) {
+			return nums2[startIndex2+k-1]
 		}
-		if index2 == len(nums2) {
-			return nums1[index2+k-1]
+		if startIndex2 == len(nums2) {
+			return nums1[startIndex1+k-1]
 		}
-		// 当 k 变为 1时，取两数组首位最小
+		// 1.2 当k为1时取两者首尾最小值
 		if k == 1 {
-			return min(nums1[index1], nums2[index2])
+			return min(nums1[startIndex1], nums2[startIndex2])
 		}
+		// 1.3 当k越界时
 		half := k / 2
-		newIndex1 := min(index1+half, len(nums1)) - 1
-		newIndex2 := min(index2+half, len(nums2)) - 1
-		pivot1, pivot2 := nums1[newIndex1], nums2[newIndex2]
-		if pivot1 <= pivot2 {
-			k -= (newIndex1 - index1 + 1)
-			index1 = newIndex1 + 1
+		index1 := min(startIndex1+half, len(nums1)) - 1
+		index2 := min(startIndex2+half, len(nums2)) - 1
+		// 2. 比较后减去小的数组长度
+		if nums1[index1] < nums2[index2] {
+			k -= index1 - startIndex1 + 1
+			startIndex1 = index1 + 1
 		} else {
-			k -= (newIndex2 - index2 + 1)
-			index2 = newIndex2 + 1
+			k -= index2 - startIndex2 + 1
+			startIndex2 = index2 + 1
 		}
 	}
 }
